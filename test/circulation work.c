62cff856e6599d327488576ff14dd7abbf18bdf9
suzhengfeng <1783113933@qq.com>
2022-05-23 20:37:09 +0800

    update

--- test/circulation work.c
+++ test/circulation work.c
@@ -157,3 +157,71 @@ int main()
 	printf("%d%d%d\n", a, b, c);
 	return 0;
 }
+
+//猜数字游戏
+//1, 电脑生成一个随机数
+//2,猜数字
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void menu()
+{
+	printf("*****************************************\n");
+	printf("**********1. play      0.exit************\n");
+	printf("*****************************************\n");
+}
+void game()
+{
+	//生成随机数
+	int ret = 0;
+	int guess = 0;
+	ret = rand()%100+1;
+	//猜数字
+	while (1)
+	{
+		printf("猜猜数字：");
+		scanf("%d", &guess);
+		if (guess > ret)
+		{
+           printf("猜大了\n");
+		}
+			
+		else if (guess < ret)
+		{
+			printf("猜小了\n");
+		}
+		else
+		{
+			printf("猜对了\n");
+			break;
+		}
+
+
+	}
+}
+int main()
+{
+	//时间戳来设置为随机数的生成起点
+    srand((unsigned int)time(NULL));
+	int input = 0;
+	do
+	{
+		menu();
+		printf("请选择：");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+				game();
+				break;
+		case 0:
+				printf("退出游戏\n");
+				break;
+		default:
+				printf("选择错误\n");
+		}
+	} while (input);
+	return 0;
+}
+
