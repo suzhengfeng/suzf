c4e8ede616ee874f98f03d931e95087daed90c4d
suzhengfeng <1783113933@qq.com>
2022-06-06 21:20:01 +0800

    arr day 2

--- test/array.c
+++ test/array.c
@@ -39,3 +39,18 @@ int main()
     }
     return 0;
 }
+
+//¾ØÕó×ªÖÃ
+#include<stdio.h>
+int main(){
+    int n,m,a[10][10],i,j;
+    scanf("%d %d",&n,&m);
+    for(i=0;i<n;i++)
+        for(j=0;j<m;j++)
+            scanf("%d",&a[i][j]);
+    for(i=0;i<m;i++){
+        for(j=0;j<n;j++)
+            printf("%d ",a[j][i]);
+        printf("\n");
+    }
+}
